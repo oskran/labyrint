a5d4691efa919f8bbdb32a66048f4fc6a961d51f
Oskar Randen <oskar.randen@gmail.com>
2020-09-10 15:42:04 +0200

    Docstring

--- SortRute.java
+++ SortRute.java
@@ -1,3 +1,6 @@
+/**
+ * Sorte ruter er vegger i labyrinten som man ikke kan gaa gjennom.
+ */
 class SortRute extends Rute {
     SortRute(Labyrint lab, int r, int kol) {
         super(lab, r, kol);
